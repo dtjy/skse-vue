643a1f65683d3d8527d642d8b2c8e4cd8fb4d4f8
dt <279964527@qq.com>
2021-01-28 17:48:12 +0800

    test

---
+++ skse-demo1/src/components/Aaa.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{ aa }}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "Aaa",
+  props: {
+    msg: String,
+  },
+  data(){
+    return {
+        aa:"Aaa子模块"
+    }
+  },
+  mounted() {
+    this.strngApplend();
+  },
+  methods: {
+    strngApplend: function () {
+      this.aa = this.msg + this.aa;
+    },
+  },
+};
+</script>
---
+++ skse-demo1/src/page/index.vue
@@ -0,0 +1,29 @@
+<template>
+
+<div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/home">index</router-link>
+      <div id="index">
+    <Aaa :msg='msg'/>
+  </div>
+    </div>
+  
+</template>
+
+
+<script>
+import Aaa from "../components/Aaa.vue"
+
+export default {
+  name: 'Index',
+  components: {
+    Aaa
+  },
+  data(){
+      return {
+          msg:"index 自定义"
+      }
+  }
+}
+</script>
