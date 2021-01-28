47168e712577119e9a9b55ebb0e676878aa61ca4
dt <279964527@qq.com>
2021-01-28 18:04:39 +0800

    test

--- skse-demo1/src/page/index.vue
+++ skse-demo1/src/page/index.vue
@@ -1,29 +1,35 @@
 <template>
-
-<div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/home">index</router-link>
-      <div id="index">
-    <Aaa :msg='msg'/>
-  </div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/home">index</router-link>
+    <div id="index">
+      <Aaa :msg="msg" />
+      <button v-on:click="counterAdd()">Add 1</button>
+      <p>{{ counter }}</p>
     </div>
-  
+  </div>
 </template>
 
 
 <script>
-import Aaa from "../components/Aaa.vue"
+import Aaa from "../components/Aaa.vue";
 
 export default {
-  name: 'Index',
+  name: "Index",
   components: {
-    Aaa
+    Aaa,
   },
-  data(){
-      return {
-          msg:"index 自定义"
-      }
-  }
-}
+  data() {
+    return {
+      msg: "index 自定义",
+      counter: 1,
+    };
+  },
+  methods: {
+    counterAdd: function () {
+      this.counter += 1;
+    },
+  },
+};
 </script>
--- skse-demo1/src/views/About.vue
+++ skse-demo1/src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
+     <router-link to="/">index</router-link>
     <h1>This is an about page</h1>
   </div>
 </template>
